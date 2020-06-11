0309d2dd2a86b579721fbc05fb9ab9aad7a4f653
Justin Hamilton <JHamilton@Pas.com>
2020-06-11 15:23:09 -0500

    Add project files.

---
+++ Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignProject
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public string User { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public int PhotoID { get; set; }
+
+    }
+}
---
+++ Photo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignProject
+{
+    public class Photo
+    {
+        public int PhotoID { get; set; }
+        public string Title { get; set; }
+        public byte PhotoFile { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Owner { get; set; }
+
+        public List <Comment> PhotoComments
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
